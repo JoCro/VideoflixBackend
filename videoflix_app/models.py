48730203c901f21dd0cb93c399b71b637b10abfb
Johannes Kroh <152779723+JoCro@users.noreply.github.com>
2025-11-27 12:55:32 +0100

    changedRequiredAdminFields

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -7,7 +7,7 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     thumbnail = models.ImageField(upload_to='thumbnails/')
-    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    video_file = models.FileField(upload_to='videos/', blank=False, null=False)
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
