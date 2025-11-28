b108966ff549eb1bcc2ba97f82f844585dea3e0a
Johannes Kroh <152779723+JoCro@users.noreply.github.com>
2025-11-28 13:12:37 +0100

    triedToFixTokenProblem

--- user_auth_app/api/authentication.py
+++ user_auth_app/api/authentication.py
@@ -1,9 +1,11 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 
 class CookieJWTAuthentication(JWTAuthentication):
     """
     Custom JWT authentication class that retrieves the token from an HTTP-only cookie.
+    If there is no token or it's invalid/expired, the request is treated as anonymous instead of raising an error. 
     """
 
     def authenticate(self, request):
@@ -13,7 +15,15 @@ class CookieJWTAuthentication(JWTAuthentication):
         else:
             raw_token = request.COOKIES.get('access_token')
 
-        if raw_token is None:
+        if not raw_token:
             return None
-        validated_token = self.get_validated_token(raw_token)
-        return self.get_user(validated_token), validated_token
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except (InvalidToken, TokenError) as e:
+            return None
+
+        try:
+            user = self.get_user(validated_token)
+        except Exception as e:
+            return None
+        return (user, validated_token)
